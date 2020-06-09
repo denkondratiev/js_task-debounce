e2e3ff007bae81a9f65431415c401a3481f83352
denkondratiev <denys.kondratiev.job@gmail.com>
2020-06-09 13:15:10 +0300

    first

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-'use strict'
+'use strict';
+
+function debounce(f, delay) {
+  let timeout;
+
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => f(...args), delay);
+  };
+}
+
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+
+function onChange(event) {
+  output.textContent = event.target.value;
+  // console.log(this.value);
+}
+
+const wrapper = debounce(onChange, 1000);
+
+input.addEventListener('input', wrapper);
---
+++ readme.md
@@ -0,0 +1,1 @@
+- [DEMO LINK](https://denkondratiev.github.io/js_task-debounce/)
